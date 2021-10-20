06c6dcaf5092b153ffeaf3add0a624fc4f1eb8f5
marcusformiga <kiko.antares@gmail.com>
2021-10-20 15:21:58 -0300

    chore: projeto inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DashBoard } from './components/DashBoard';
+import { Header } from './components/Header';
+import { GlobalStyle } from './styles/global';
+import Modal from "react-modal"
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
-function App() {
+Modal.setAppElement("#root")
+export function App() {
+   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+     useState(false);
+   function handleOpenNewModal() {
+     setIsNewTransactionModalOpen(true);
+   }
+   function handleCloseNewTransactionModal() {
+     setIsNewTransactionModalOpen(false);
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewModal}/>
+      <DashBoard />
+      <GlobalStyle />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={ handleCloseNewTransactionModal}/>
+    </>
   );
 }
 
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {App} from './App';
+import { createServer, Model } from "miragejs"
 
+createServer({
+  models: {
+    transaction: Model
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance website",
+          type: "deposit",
+          category: "Desenvolvimento",
+          value: 5000,
+          createdAt: new Date("2021-08-31 17:00:00"),
+        },
+        {
+          id: 2,
+          title: "Alugel de apartamento",
+          type: "withdraw",
+          category: "Aluguel",
+          value: 1000,
+          createdAt: new Date("2021-08-31 17:00:00"),
+        },
+      ],
+    });
+  },
+  routes() {
+    this.namespace = "api"
+    this.get("/transactions", () => {
+     return this.schema.all("transactions")
+    })
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create("transactions", data)
+    })
+  }
+})
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,7 +47,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
